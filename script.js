cb1c3c6778807ac563d40c717b7e3e87878f190e
Julien Flusin <flusin@apside.fr>
2023-03-27 13:36:37 +0200

    1ère version de la page de login / register avec les requetes AJAX

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginTab = document.getElementById('login-tab')
-    const registerTab = document.getElementById('register-tab')
-    const loginForm = document.getElementById('login-form')
-    const registerForm = document.getElementById('register-form')
-    loginTab.addEventListener('click', () => {
-        loginTab.classList.add('bg-blue-500', 'text-white')
-        registerTab.classList.remove('bg-blue-500', 'text-white')
-        registerTab.classList.add('text-blue-500')
-        loginForm.classList.remove('hidden')
-        registerForm.classList.add('hidden')
-    })
-    registerTab.addEventListener('click', () => {
-        registerTab.classList.add('bg-blue-500', 'text-white')
-        loginTab.classList.remove('bg-blue-500', 'text-white')
-        loginTab.classList.add('text-blue-500')
-        registerForm.classList.remove('hidden')
-        loginForm.classList.add('hidden')
-    })
-    loginForm.addEventListener('submit', e => {
-        e.preventDefault()
-        const email = document.getElementById('login-email').value
-        const password = document.getElementById('login-password').value
-        // Ajoutez ici le code pour soumettre les données de connexion via AJAX
-    })
-    registerForm.addEventListener('submit', e => {
-        e.preventDefault()
-        const email = document.getElementById('register-email').value
-        const password = document.getElementById('register-password').value
-        const confirmPassword = document.getElementById('register-confirm-password').value
+loginTab = document.getElementById("login-tab");
+registerTab = document.getElementById("register-tab");
+loginForm = document.getElementById("login-form");
+registerForm = document.getElementById("register-form");
 
-        if (password !== confirmPassword) {
-          alert('Les mots de passe ne correspondent pas.')
-          return
-        }
-        // Ajoutez ici le code pour soumettre les données d'inscription via AJAX
+loginTab.addEventListener("click", function () {
+    loginForm.classList.remove("hidden");
+    registerForm.classList.add("hidden");
+    this.classList.add("bg-blue-500", "text-white");
+    registerTab.classList.remove("bg-blue-500", "text-white");
+    registerTab.classList.add("text-blue-500");
+});
+loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let email = document.getElementById("login-email").value;
+    let password = document.getElementById("login-password").value;
+    let data = {
+        email: email,
+        password: password,
+    };
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
     })
-})  
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.status == "success") {
+                window.location.href = "/dashboard";
+            } else {
+                alert(data.message);
+            }
+        });
+});
+
+
+registerTab.addEventListener("click", function () {
+    registerForm.classList.remove("hidden");
+    loginForm.classList.add("hidden");
+    this.classList.add("bg-blue-500", "text-white");
+    loginTab.classList.remove("bg-blue-500", "text-white");
+    loginTab.classList.add("text-blue-500");
+});
+
+registerForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let email = document.getElementById("register-email").value;
+    let password = document.getElementById("register-password").value;
+    let data = {
+        email: email,
+        password: password,
+    };
+    fetch("/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.status == "success") {
+                window.location.href = "/dashboard";
+            } else {
+                alert(data.message);
+            }
+        });
+});
